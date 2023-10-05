570b18362e41cb50f7ae085d122e86acd908af84
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2023-10-05 15:03:50 +0300

    Initial project setup

---
+++ src/test/java/com/streamlined/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamlined.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
